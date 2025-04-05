c9b6365ed45ef3f3bdfdd00ec200bd8c3e60d949
sanjay <117289265+sanjayexe@users.noreply.github.com>
2025-04-05 11:02:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,23 +5,23 @@ echo "🚀 Starting Deployment..."
 
 # 🔑 Login to Docker Hub securely
 echo "🔑 Logging into Docker Hub..."
-echo "Virat@18vk" | docker login -u "sanjay188" -p "Virat@18vk"
+echo "Virat@18vk" | sudo docker login -u "sanjay188" -p "Virat@18vk"
 
 # 🐳 Build and Push Docker Image
 echo "🐳 Building Docker Image..."
-docker build -t sanjay188/devops:latest .
+sudo docker build -t sanjay188/devops:latest .
 
 echo "📤 Pushing Image to Docker Hub..."
-docker push sanjay188/devops:latest
+sudo docker push sanjay188/devops:latest
 
 
 
 # 🗑 Clean old images
 echo "🗑 Removing old Docker images..."
-docker image prune -f
+sudo docker image prune -f
 
 # 🚀 Run New Container
 echo "🚀 Deploying New Container..."
-docker run -d -p 8000:5000 --name=my-service-repo sanjay188/devops:latest
+sudo docker run -d -p 8000:5000 --name=my-service-repo sanjay188/devops:latest
 
 echo "✅ Deployment Successful!"
